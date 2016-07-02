4f134845de7e7b57cbc0c786bc0d89750992cdf8
codehumane <codehumane@gmail.com>
2016-07-02 17:42:41 +0900

    use cache as graph

---
+++ src/main/java/data/CacheGraph.java
@@ -0,0 +1,137 @@
+package data;
+
+import com.google.common.collect.Lists;
+import org.springframework.cache.Cache;
+
+import java.io.Serializable;
+import java.util.List;
+
+//import org.springframework.cache.Cache;
+//import org.springframework.cache.Cache.ValueWrapper;
+//
+//import com.google.common.collect.Lists;
+
+/**
+ * Cache를 그래프 형태로 관리
+ *
+ * edge: 간선 <br/>
+ * vertex: 정점 (노드) <br/>
+ *
+ */
+public class CacheGraph<T> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 데이터 source
+     */
+    private final Cache cache;
+
+    public CacheGraph(Cache cache) {
+        this.cache = cache;
+    }
+
+    /**
+     * 간선 추가
+     *
+     * @param from
+     * @param to
+     * @return
+     */
+    public boolean addEdge(T from, T to) {
+        if (cache == null) {
+            return false;
+        }
+
+        boolean linkedTo = link(new CacheGraphVertex<>(from), new CacheGraphVertex<>(to));
+        boolean linkedFrom = link(new CacheGraphVertex<>(to), new CacheGraphVertex<>(from));
+
+        /**
+         * 캐시 만료 정책으로 인해 A → B 연결은 존재하지만 B → A로의 연결은 부재할 수 있다. <br/>
+         * 이에 따라 한쪽으로라도 연결이 이루어지면 연결 성공이라고 간주한다. <br/>
+         */
+        return linkedTo || linkedFrom;
+    }
+
+    @SuppressWarnings("unchecked")
+    private boolean link(CacheGraphVertex<T> from, CacheGraphVertex<T> to) {
+        Cache.ValueWrapper valueWrapper = cache.get(from);
+        List<CacheGraphVertex<T>> vertexes = Lists.newArrayList();
+        if (valueWrapper != null) {
+            vertexes = (List<CacheGraphVertex<T>>) valueWrapper.get();
+        }
+
+        if (vertexes.contains(to)) {
+            return false;
+        }
+
+        vertexes.add(to);
+        cache.put(from, vertexes);
+        return true;
+    }
+
+    /**
+     * 간선 제거
+     *
+     * @param from
+     * @param to
+     * @return
+     */
+    public boolean removeEdge(T from, T to) {
+        if (cache == null) {
+            return false;
+        }
+
+        boolean unlinked = unlink(new CacheGraphVertex<>(from), new CacheGraphVertex<>(to));
+        unlink(new CacheGraphVertex<>(to), new CacheGraphVertex<>(from));
+        return unlinked;
+    }
+
+    @SuppressWarnings("unchecked")
+    private boolean unlink(CacheGraphVertex<T> from, CacheGraphVertex<T> to) {
+        Cache.ValueWrapper valueWrapper = cache.get(from);
+        if (valueWrapper == null) {
+            return false;
+        }
+
+        List<CacheGraphVertex<T>> vertexes = (List<CacheGraphVertex<T>>) valueWrapper.get();
+        if (!vertexes.contains(to)) {
+            return false;
+        }
+
+        vertexes.remove(to);
+        if (!vertexes.isEmpty()) {
+            cache.put(from, vertexes);
+        } else {
+            cache.evict(from);
+        }
+
+        return true;
+    }
+
+    /**
+     * 인접 정점 모두 반환
+     *
+     * @param from
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public List<T> getAdjacentVertexes(T from) {
+        if (cache == null) {
+            return Lists.newArrayList();
+        }
+
+        Cache.ValueWrapper valueWrapper = cache.get(new CacheGraphVertex<>(from));
+        if (valueWrapper == null) {
+            return Lists.newArrayList();
+        }
+
+        List<T> list = Lists.newArrayList();
+        for (CacheGraphVertex<T> vertex : (List<CacheGraphVertex<T>>) valueWrapper.get()) {
+            list.add(vertex.getValue());
+        }
+
+        return list;
+    }
+
+}
